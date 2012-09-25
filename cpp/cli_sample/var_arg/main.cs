3398c835b4f0b98dd117acf0266b9340638712ce
qtakamitsu <qtakamitsu@gmail.com>
2012-09-26 00:38:49 +0900

    miss main.cs

---
+++ cpp/cli_sample/var_arg/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        Foo f = new Foo();
+
+        Console.WriteLine("TestInt = {0}", f.TestInt(1, 2, 3, 4, 5));
+
+        f.TestString("aaa", "bbb", "ccc");
+
+    }
+}
+
