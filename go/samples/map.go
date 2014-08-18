bc6c55dc9c76b905e0a72ba5fa7a7fb046ad6b1d
qtakamitsu <qtakamitsu@gmail.com>
2014-08-18 22:39:26 +0900

    modify map.go

--- go/samples/map.go
+++ go/samples/map.go
@@ -9,5 +9,21 @@ func main() {
 	}
 
 	fmt.Println(m)
+
+	v1 := m["key01"]
+	fmt.Println(v1)
+
+	_, is_exists := m["key_99"]
+	fmt.Println("exists: ", is_exists)
+
+
+	m["key03"] = "val03"
+	m["key04"] = "val04"
+	fmt.Println(m)
+
+	m["key03"] = "val03"
+
+	delete(m, "key02")
+	fmt.Println(m)
 }
 
