c65a352cc03b7eb7aeb7effc8435d9d3475386fd
qtakamitsu <qtakamitsu@gmail.com>
2014-08-20 23:19:30 +0900

    add http_server.go

---
+++ go/samples/http_server.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "net/http"
+
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
+
