48c54f92d0ea8b3a88ad31a570e767902bfb838a
qtakamitsu <qtakamitsu@gmail.com>
2014-08-21 23:44:36 +0900

    modify http_server.go

--- go/samples/http_server.go
+++ go/samples/http_server.go
@@ -3,12 +3,15 @@ package main
 import "fmt"
 import "net/http"
 
-
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "root")
+	})
+
 	http.HandleFunc("/hello", HelloHandler)
 
 	http.ListenAndServe(":8080", nil)
