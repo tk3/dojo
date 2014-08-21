8753c9974b166e2b016f09d05f930238a12408c7
qtakamitsu <qtakamitsu@gmail.com>
2014-08-21 23:53:39 +0900

    modify http_server.go

--- go/samples/http_server.go
+++ go/samples/http_server.go
@@ -8,11 +8,20 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	echo_handler := func(w http.ResponseWriter, r *http.Request) {
+		for name, value := range r.Header {
+			fmt.Fprintf(w, "%s=%s\n", name, value[0])
+			fmt.Println(value[0])
+		}
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "root")
 	})
 
 	http.HandleFunc("/hello", HelloHandler)
+
+	http.HandleFunc("/echo", echo_handler)
 
 	http.ListenAndServe(":8080", nil)
 }
