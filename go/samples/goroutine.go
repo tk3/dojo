6c556d51120bc27dd539b038a2b9205354c6f01f
qtakamitsu <qtakamitsu@gmail.com>
2014-08-25 23:12:35 +0900

    modify goroutine.go

--- go/samples/goroutine.go
+++ go/samples/goroutine.go
@@ -1,15 +1,22 @@
 package main
 
 import "fmt"
+import "sync"
 
 var total int
 
-func func1(n int) {
+func func1(n int, wg *sync.WaitGroup) {
 	total += n
+	wg.Done()
 }
 
 func main() {
-	go func1(10)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func1(10, &wg)
+
+	wg.Wait()
 
 	fmt.Println(total)
 }
