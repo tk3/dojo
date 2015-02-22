ddf7ad3ee95a3e13ff8ce49282f801ab8aa2de87
qtakamitsu <qtakamitsu@gmail.com>
2015-02-22 05:50:52 +0000

    ngx_mruby add basic auth sample

---
+++ mruby/ngx_mruby/basic_auth/src/basic_auth.rb
@@ -0,0 +1,35 @@
+def basic_auth
+  r = Nginx::Request.new
+  r.headers_out["WWW-Authenticate"] = 'Basic realm="Private Page"'
+
+  if r.headers_in["Authorization"].nil?
+    return Nginx::HTTP_UNAUTHORIZED
+  end
+
+  auth = r.headers_in["Authorization"]
+  params = Base64::decode(auth.split(" ")[1]).split(":")
+
+  unless yield(params[0], params[1])
+    return Nginx::HTTP_UNAUTHORIZED
+  end
+
+  return Nginx::DECLINED
+end
+
+def load_user_list(file)
+  users = {}
+  IO.open(IO.sysopen((file))) {|io|
+    io.each {|line|
+      params = line.chomp.split("\t")
+      users[params[0]] = params[1] if 1 < params.size
+    }
+  }
+  users
+end
+
+r = Nginx::Request.new
+users = load_user_list(r.var.user_list)
+
+Nginx.return basic_auth {|id, password|
+  users.key?(id) && users[id] == password
+}
