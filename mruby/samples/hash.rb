cd38ef30f4b9798035692005a28a99ac055b1f87
qtakamitsu <qtakamitsu@gmail.com>
2015-02-25 17:18:46 +0000

    mruby modify plain

--- mruby/samples/hash.rb
+++ mruby/samples/hash.rb
@@ -1,3 +1,6 @@
+exit(0) if ARGV.length < 1
+
+puts "PLAIN : " + ARGV[0]
 puts "MD5   : " + Base64::encode(Digest::MD5.digest(ARGV[0]))
 puts "SHA1  : " + Base64::encode(Digest::SHA1.digest(ARGV[0]))
 puts "SHA256: " + Base64::encode(Digest::SHA256.digest(ARGV[0]))
