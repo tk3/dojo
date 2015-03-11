31de5fa5895ac20c27dbd4674333545697417356
qtakamitsu <qtakamitsu@gmail.com>
2015-03-11 11:40:39 +0000

    fix md5_crypto

--- mruby/samples/md5_crypto.rb
+++ mruby/samples/md5_crypto.rb
@@ -1,127 +1,103 @@
+class MD5Crypt
+  def self.encrypt(password, hashed_hpassword)
+    return ""  unless hashed_hpassword =~ /(\$.+\$)(.+)\$(.+)/
+    key = $1
+    salt = $2
+    hash = $3
 
-def int_to_bitstring(n)
-  v = ''
-  n.each do |i|
-    v << "%08b" % i
-  end
-  v
-end
+    # hash #1
+    h1 = Digest::MD5.new
+    h1.update(password)
+    h1.update(key)
+    h1.update(salt)
 
-def bitstring_to_int(s)
-  v = 0
-  s.each_char do |i|
-    v <<= 1
-    if i == "1"
-      v += 1
-    end
-  end
-  v
-end
+    # hash #2
+    h2 = Digest::MD5.new
+    h2.update(password)
+    h2.update(salt)
+    h2.update(password)
+    final = h2.digest
 
-def encode64(s)
-  permutate_tbl = [0, 3, 6, 9, 12, 14, 1, 4, 7, 10, 13, 15, 2, 5, 8, 11]
-
-  tmp1 = Array.new(16)
-  s.unpack("C*").each_with_index do |v, i|
-    tmp1[permutate_tbl[i]] = v
-  end
-
-  bit_string = int_to_bitstring(tmp1)
-puts bit_string
-  remainder = bit_string.size % 6
-  if 0 < remainder
-    bit_string += "0" * (6 - remainder)
-  end
-
-  ascii_tbl = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  r = ""
-  0.step(bit_string.length - 1, 6) do |n|
-    item = bit_string[n, 6]
-    tbl_index = bitstring_to_int("00" + item)
-    r << ascii_tbl[tbl_index]
-  end
-
-  r
-end
-
-def md5_crypto(password, hashed_hpassword)
-  return ""  unless hashed_hpassword =~ /(\$.+\$)(.+)\$(.+)/
-  key = $1
-  salt = $2
-  hash = $3
-
-  puts "key: #{key}"
-  puts "salt: #{salt}"
-  puts "hash: #{hash}"
-
-  # hash #1
-  h1 = Digest::MD5.new
-  h1.update(password)
-  h1.update(key)
-  h1.update(salt)
-
-  # hash #2
-  h2 = Digest::MD5.new
-  h2.update(password)
-  h2.update(salt)
-  h2.update(password)
-  final = h2.digest
-
-  i = password.length
-  while 0 < i
-    if i < 16
-      h1.update(final[0, i])
-    else
-      h1.update(final)
-    end
-    i -= 16
-  end
-
-  i = password.length
-  while 0 < i
-    if i & 1 == 1
-      h1.update("\0")
-    else
-      h1.update(password[0])
-    end
-    i >>= 1
-  end
-  final = h1.digest
-
-  1000.times do |i|
-    h = Digest::MD5.new
-
-    if i & 1 == 1
-      h.update(password)
-    else
-      h.update(final)
+    i = password.length
+    while 0 < i
+      if i < 16
+        h1.update(final[0, i])
+      else
+        h1.update(final)
+      end
+      i -= 16
     end
 
-    if i % 3 > 0
-      h.update(salt)
+    i = password.length
+    while 0 < i
+      if i & 1 == 1
+        h1.update("\0")
+      else
+        h1.update(password[0])
+      end
+      i >>= 1
+    end
+    final = h1.digest
+
+    1000.times do |i|
+      h = Digest::MD5.new
+
+      if i & 1 == 1
+        h.update(password)
+      else
+        h.update(final)
+      end
+
+      if i % 3 > 0
+        h.update(salt)
+      end
+
+      if i % 7 > 0
+        h.update(password)
+      end
+
+      if i & 1 == 1
+        h.update(final)
+      else
+        h.update(password)
+      end
+
+      final = h.digest
     end
 
-    if i % 7 > 0
-      h.update(password)
-    end
-
-    if i & 1 == 1
-      h.update(final)
-    else
-      h.update(password)
-    end
-
-    final = h.digest
+    self.encode64(final)
   end
 
-  encode64(final)
+  def self.encode64(s)
+    ascii_tbl = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    a = s.unpack("C*")
+
+    tmp = []
+    tmp << ((a[0]<<16) | (a[6]<<8) | a[12])
+    tmp << ((a[1]<<16) | (a[7]<<8) | a[13])
+    tmp << ((a[2]<<16) | (a[8]<<8) | a[14])
+    tmp << ((a[3]<<16) | (a[9]<<8) | a[15])
+    tmp << ((a[4]<<16) | (a[10]<<8) | a[5])
+    tmp << ((1<<12) | a[11])
+
+    r = ""
+    tmp.each do |i|
+      n = i
+      while 0 < n
+        r << ascii_tbl[n & 0x3f]
+        n >>= 6
+      end
+    end
+    r[0, 22]
+  end
 end
 
 s = 'user02:$apr1$7EkYqIlu$zP0IETYrGdxLUFGzyvRQs1'
 s = 'user01:$apr1$K5S1J1Cw$bT/200cBbpdVb6Hp8vu3W.'
 
 p = s.split(":")
-
 user = p[0]
+r = MD5Crypto.encrypt("user01", p[1])
+puts r
 
-puts md5_crypto("user01", p[1])
