5058aa146ff2be9324a826ba5426c443aef31354
qtakamitsu <qtakamitsu@gmail.com>
2015-10-04 00:25:42 +0900

    modify for ubuntu

--- build/rbenv_ruby_2.1.5.sh
+++ build/rbenv_ruby_2.1.5.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo yum -y install gcc readline-devel libyaml-devel openssl-devel
+# sudo apt install -y libreadline-dev libyaml-dev libssl-dev
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
