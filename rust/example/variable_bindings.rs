e879a3ac5f95713aae4ee40fa7a4a73fd00c6cd2
qtakamitsu <qtakamitsu@gmail.com>
2016-05-07 16:41:52 +0900

    add rust example; variable_bindings.rs

---
+++ rust/example/variable_bindings.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("a boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    let _unused_variable = 3u32;
+
+    let noisy_unsed_variable = 2u32;
+}
