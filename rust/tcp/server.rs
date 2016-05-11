4f7424c40ff876bf40b3e918fafd36ae00fe0da7
qtakamitsu <qtakamitsu@gmail.com>
2016-05-11 23:45:02 +0900

    modify

--- rust/tcp/server.rs
+++ rust/tcp/server.rs
@@ -6,7 +6,6 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     fn handle_client(mut stream: TcpStream) {
-//    fn handle_client(stream: &mut TcpStream) {
         let _ = stream.write(b"Hello");
     }
 
@@ -14,11 +13,9 @@ fn main() {
         match stream {
             Ok(stream) => {
                 thread::spawn(move|| {
-                    //handle_client(&mut stream);
                     handle_client(stream);
                 });
             }
-            //Err(e) => { /* connection failed */ }
             Err(e) => {
                 println!("Error: {}", e);
             },
