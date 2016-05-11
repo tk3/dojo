9e45488c9317a45e80b13c2ef4e1a58fdd9d90ea
qtakamitsu <qtakamitsu@gmail.com>
2016-05-12 00:50:16 +0900

    modify

--- rust/tcp/client.rs
+++ rust/tcp/client.rs
@@ -6,15 +6,16 @@ fn main() {
     {
         let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
 
-        let _ = stream.write(&[1]);
+        //let _ = stream.write(&[1]);
+        let _ = stream.write(b"OkOkOk");
 
         let mut line;
         line = [0; 128];
         let read_byte = stream.read(&mut line);
         match read_byte {
             Ok(n) => {
-                println!("Received {} bytes", n);
-                println!(">> [{}]", str::from_utf8(&line[0..n]).unwrap());
+                println!("client) Received {} bytes", n);
+                println!("client) >> [{}]", str::from_utf8(&line[0..n]).unwrap());
             },
             _ => {},
         }
--- rust/tcp/server.rs
+++ rust/tcp/server.rs
@@ -1,11 +1,23 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::Read;
 use std::io::Write;
 use std::thread;
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     fn handle_client(mut stream: TcpStream) {
+        let mut line;
+        line = [0; 128];
+        let read_byte = stream.read(&mut line);
+        match read_byte {
+            Ok(n) => {
+                println!("server) Received {} bytes", n);
+                println!("server) >> [{}]", str::from_utf8(&line[0..n]).unwrap());
+            },
+            _ => {},
+        }
         let _ = stream.write(b"Hello");
     }
 
@@ -17,7 +29,7 @@ fn main() {
                 });
             }
             Err(e) => {
-                println!("Error: {}", e);
+                println!("server) Error: {}", e);
             },
         }
     }
