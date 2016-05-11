f70e619adc7fb51eba12dce4637e8222db14125c
qtakamitsu <qtakamitsu@gmail.com>
2016-05-12 00:43:22 +0900

    modify

--- rust/tcp/client.rs
+++ rust/tcp/client.rs
@@ -1,18 +1,21 @@
 fn main() {
     use std::io::prelude::*;
     use std::net::TcpStream;
+    use std::str;
 
     {
         let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
 
-        // ignore the Result
         let _ = stream.write(&[1]);
 
         let mut line;
         line = [0; 128];
         let read_byte = stream.read(&mut line);
         match read_byte {
-            Ok(n) => println!("Received {} bytes", n),
+            Ok(n) => {
+                println!("Received {} bytes", n);
+                println!(">> [{}]", str::from_utf8(&line[0..n]).unwrap());
+            },
             _ => {},
         }
     }
