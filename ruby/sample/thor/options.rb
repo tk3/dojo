845a71b6bfd998c69c9ea39161ad5a5cf4ebd6da
qtakamitsu <qtakamitsu@gmail.com>
2017-02-28 12:48:54 +0000

    modify options.rb

--- ruby/sample/thor/options.rb
+++ ruby/sample/thor/options.rb
@@ -3,15 +3,51 @@
 require "thor"
 
 class MyCLI < Thor
-  option :param1
-  option :param2, :type => :boolean
-  option :param3, :desc => "description", :banner => "banner text"
-  option :param4, :default => "abcd"
-  option :param5, :required => true
-  option :param6, :aliases => "-x"
-	option :param7, type: :numeric, :enum => [10, 20]
-  desc "hello", "command description"
-  def hello
+  desc "case1", "command description"
+  option :param
+  def case1
+    puts options
+  end
+
+  desc "case2", "command description"
+  option :param, :type => :boolean
+  def case2
+    puts options
+  end
+
+  desc "case3", "command description"
+  option :param, :desc => "description", :banner => "banner text"
+  def case3
+    puts options
+  end
+
+  desc "case4", "command description"
+  option :param, :default => "abcd"
+  def case4
+    puts options
+  end
+
+  desc "case5", "command description"
+  option :param, :required => true
+  def case5
+    puts options
+  end
+
+  desc "case6", "command description"
+  option :param, :aliases => "-x"
+  def case6
+    puts options
+  end
+
+  desc "case7", "command description"
+  option :param, type: :numeric, :enum => [10, 20]
+  def case7
+    puts options
+  end
+
+  desc "case8", "command description"
+  option :param #, :lazy_default => true
+  def case8
     puts options
   end
 end
