059dbe786922d7204e4597898a37b8c919611391
qtakamitsu <qtakamitsu@gmail.com>
2017-03-15 13:36:32 +0000

    add

--- mruby/ffi/test/test_mrb_ffi.rb
+++ mruby/ffi/test/test_mrb_ffi.rb
@@ -20,11 +20,19 @@ assert "test-3" do
   end
 end
 
-#assert "test-2" do
-#  assert_raise(ArgumentError) do
-#    libc = "/lib/x86_64-linux-gnu/libc.so.6"
-#    dl = FFI::DynamicLibrary.new libc, 1
-#    dl.find :aaa, [:bbb], :ccc
-#  end
-#end
+assert "test-4" do
+  assert_raise(ArgumentError) do
+    libc = "/lib/x86_64-linux-gnu/libc.so.6"
+    dl = FFI::DynamicLibrary.new libc, 1
+    f = dl.find :atoi, [:string], :int
+    f.call 10, 20
+  end
+end
+
+assert "test-5" do
+  libc = "/lib/x86_64-linux-gnu/libc.so.6"
+  dl = FFI::DynamicLibrary.new libc, 1
+  f = dl.find :atoi, [:string], :int
+  assert_equal f.call(1234), 1234
+end
 
