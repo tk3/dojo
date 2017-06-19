58cdc21c207089ba4c444533456c39ef88b89096
qtakamitsu <qtakamitsu@gmail.com>
2017-06-20 08:38:50 +0900

    modify match sample

--- rust/sample/match.rs
+++ rust/sample/match.rs
@@ -1,11 +1,32 @@
 fn main() {
+    sample1();
+    sample2();
+}
+
+fn sample1() -> () {
+    println!("sample 1");
 
     for n in 0..10 {
         let r = match n {
           0           => 1,
-          t @ 2       => t + 1,
+          t @ 2       => t + 10,
           n if n < 5  => 5,
           _           => 6
+        };
+        println!("{} -> {}", n, r);
+    }
+}
+
+fn sample2() -> () {
+    println!("sample 2");
+
+    for n in 0..3 {
+        let r = match n {
+          t @ 2 => {
+              let a = t + 10;
+              a
+          },
+          _     => 6
         };
         println!("{} -> {}", n, r);
     }
