10533b6ce4578ce079c2f056b0a2bf19c87b7d50
qtakamitsu <qtakamitsu@gmail.com>
2017-07-01 11:20:39 +0900

    add testcase

--- rust/semantic-versioning/src/main.rs
+++ rust/semantic-versioning/src/main.rs
@@ -8,3 +8,14 @@ struct Version {
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(0, 0);
+    }
+}
+
