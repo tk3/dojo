5a397b8eb6c8825713fda694e3f7a9c64ba1980a
qtakamitsu <qtakamitsu@gmail.com>
2017-07-01 19:02:09 +0900

    add testcase

--- rust/semantic-versioning/src/main.rs
+++ rust/semantic-versioning/src/main.rs
@@ -5,6 +5,12 @@ struct Version {
     patch: i32,
 }
 
+impl Version {
+    pub fn to_string(&self) -> String {
+        format!("{}.{}.{}", self.major, self.minor, self.patch)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -20,6 +26,13 @@ mod test {
         assert_eq!(v.major, 1);
         assert_eq!(v.minor, 4);
         assert_eq!(v.patch, 2);
+    }
+
+    #[test]
+    fn version_to_string_works() {
+        let v: Version = Version { major: 1, minor: 4, patch: 2 };
+
+        assert_eq!(v.to_string(), "1.4.2");
     }
 }
 
