08d5e087490b1e97234d476a3ea91eb19e4d70d0
qtakamitsu <qtakamitsu@gmail.com>
2017-07-02 22:45:28 +0900

    add equal testcase

--- go/samples/semantic-versioning/main_test.go
+++ go/samples/semantic-versioning/main_test.go
@@ -17,3 +17,17 @@ func TestVersionStringWorks(t *testing.T) {
 		t.Error("Version ToString() test failed.")
 	}
 }
+
+func TestVersionEqualWorks(t *testing.T) {
+	v1 := Version{Major: 1, Minor: 4, Patch: 2}
+	v2 := Version{Major: 1, Minor: 4, Patch: 2}
+	v3 := Version{Major: 2, Minor: 1, Patch: 0}
+
+	if !(v1 == v2) {
+		t.Error("Version Equal test failed.")
+	}
+
+	if !(v1 != v3) {
+		t.Error("Version Not Equal test failed.")
+	}
+}
