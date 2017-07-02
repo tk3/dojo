b29527a1233b5e77c99882ec449f358ba5261b70
qtakamitsu <qtakamitsu@gmail.com>
2017-07-02 22:13:55 +0900

    modify test data

--- go/samples/semantic-versioning/main_test.go
+++ go/samples/semantic-versioning/main_test.go
@@ -5,15 +5,15 @@ import (
 )
 
 func TestVersionInitializeWorks(t *testing.T) {
-	v := Version{Major: 1, Minor: 4, Patch: 0}
-	if !(v.Major == 1 && v.Minor == 4 && v.Patch == 0) {
+	v := Version{Major: 1, Minor: 4, Patch: 2}
+	if !(v.Major == 1 && v.Minor == 4 && v.Patch == 2) {
 		t.Error("Version initialize test failed.")
 	}
 }
 
 func TestVersionStringWorks(t *testing.T) {
-	v := Version{Major: 1, Minor: 4, Patch: 0}
-	if !(v.ToString() == "1.4.0") {
+	v := Version{Major: 1, Minor: 4, Patch: 2}
+	if !(v.ToString() == "1.4.2") {
 		t.Error("Version ToString() test failed.")
 	}
 }
