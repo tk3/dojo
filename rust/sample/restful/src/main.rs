c54517bbfa59b99b302412fa869f3ab3978b7d7d
qtakamitsu <qtakamitsu@gmail.com>
2017-12-28 00:25:19 +0900

    add

---
+++ rust/sample/restful/src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+extern crate rustful;
+use std::error::Error;
+use rustful::{Server, Context, Response};
+
+fn main() {
+    env_logger::init().unwrap();
+
+    println!("Visit http://localhost:8080 to try this example.");
+    let server_result = Server {
+            host: 8080.into(),
+            ..Server::new(|_: Context, res: Response| res.send("Hello!"))
+    }.run();
+
+    match server_result {
+        Ok(_server) => {},
+        Err(e) => error!("could not start server: {}", e.description())
+    }
+}
