e780d36e34742041a71b877a68dc655e6688e676
qtakamitsu <qtakamitsu@gmail.com>
2018-01-11 23:25:32 +0900

    modify

--- rust/sample/env_args/src/main.rs
+++ rust/sample/env_args/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in env::args() {
+        println!("{}", arg);
+    }
+
+    let args: Vec<String> = env::args().collect();
+    println!("count: {}", args.len());
+    for i in 0..args.len() {
+        println!("args[{}] = {}", i, args[i]);
+    }
+
+    let n = env::args().skip(1).filter(|arg| arg.len() > 3).count();
+    println!("match: {:?}", n);
 }
