95a2cb39f26ed8156980f796b810f197210aaf9b
qtakamitsu <qtakamitsu@gmail.com>
2018-01-12 21:19:41 +0900

    update

--- rust/sample/sample_regex/src/main.rs
+++ rust/sample/sample_regex/src/main.rs
@@ -7,7 +7,7 @@ fn sample1() {
     match r {
         Ok(n) => {
             println!("OK: {}", n);
-            assert!(n.is_match("Hello, world!"))
+            assert_eq!(n.is_match("Hello, world!"), true)
         }
         Err(e) => println!("NG: {}", e),
     }
@@ -20,10 +20,20 @@ fn sample2() {
         Ok(n) => println!("OK: {}", n),
         Err(e) => println!("NG: {}", e),
     }
+
+}
+
+fn sample3() {
+    let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
+    let text = "2012-03-14, 2013-01-01 and 2014-07-05";
+    for cap in re.captures_iter(text) {
+        println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
+    }
 }
 
 fn main() {
     sample1();
     sample2();
+    sample3();
 }
 
