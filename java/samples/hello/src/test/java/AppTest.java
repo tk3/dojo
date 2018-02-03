5c2a3478247fe98be942ce59d3bdab0641f62ba5
qtakamitsu <qtakamitsu@gmail.com>
2018-02-04 00:24:00 +0900

    add hello

---
+++ java/samples/hello/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+//import com.example;
+
+/*
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
+*/
