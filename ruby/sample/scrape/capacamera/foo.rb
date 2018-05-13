c6003381a8bfad017eee296b0439d818c0ce35ae
qtakamitsu <qtakamitsu@gmail.com>
2018-05-13 13:26:15 +0900

    add foo.rb

--- ruby/sample/scrape/capacamera/foo.rb
+++ ruby/sample/scrape/capacamera/foo.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-require "faraday"
 require "nokogiri"
 
 class Scraper
@@ -10,39 +9,67 @@ class Scraper
 
       ctx = {}
       data = []
-      doc.css("#column").children.each do |e|
+
+      doc.css("#main .entry-content").children.each do |e|
         case e.name
-        when "h2"
-          ctx[:title] = e.text
-        when "h3"
+        when /h2/
+          ctx = {}
           ctx[:area] = e.text
-        when "p"
+        when /h3/
           ctx[:category] = e.text
-        when "h4"
-          ctx[:building_name] = e.text
-          ctx[:building_url] = e.css("a").attr("href").value
-        when "h6"
-          ctx[:room_name] = e.text
-        when "table"
-          e.css("tr").each do |tr|
-            td = tr.css "td"
-            ctx[:exhibition_name] = td[0].text
-            ctx[:held_for_days] = td[1].text
+        when /section/
+          ctx[:name] = e.css("div.place h4").text
+          ctx[:url] = e.css("div.place li:nth-of-type(1) a")[0].attr("href")
+          ctx[:map] = e.css("div.place li:nth-of-type(2) a")[0].attr("href")
+          ctx[:description] = e.css("div.place p").text.strip
+
+          # exhibition
+          e.css("> ul li").each do |exhibition|
+            ctx[:exhibition_title] = exhibition.css("div.date").text
+            ctx[:exhibition_period] = exhibition.css("p").text
+
+            data << ctx.dup
           end
-          data << ctx.dup
         end
       end
+
       data
     end
   end
 end
 
-#u = "http://capacamera.net/exhibition/04.html"
-#r = Faraday.get u
-#html = r.body
 
-html = File.read 'capacamera.cache.txt'
+if __FILE__ == $0
 
-puts Scraper::CAPACameranet.scrape html
+  require "faraday"
 
+  cache_file = "./capacamera.cache.txt"
+
+  html = ""
+  unless File.exist?(cache_file)
+    url = "https://capa.getnavi.jp/exhibition/tokyo/"
+    r = Faraday.get u
+    html = r.body
+
+    File.open(cache_file, "w") do |fout|
+      fout.puts html
+    end
+  else
+    html = File.read 'capacamera.cache.txt'
+  end
+
+  data = Scraper::CAPACameranet.scrape html
+
+  data.each do |d|
+    puts "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % [
+          d[:area],
+          d[:category],
+          d[:name],
+          d[:url],
+          d[:map],
+          d[:description],
+          d[:exhibition_title],
+          d[:exhibition_period]]
+  end
+end
 
