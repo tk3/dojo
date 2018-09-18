ebec149d274f0f7e87af15a9462f45f677d27f3e
qtakamitsu <qtakamitsu@gmail.com>
2018-09-18 23:52:35 +0900

    add file

---
+++ shell/backlog-api/bashlog-api.sh
@@ -0,0 +1,23 @@
+
+test -f ./.env && source ./.env
+
+echo "BACKLOG_URL     = $BACKLOG_URL"
+echo "BACKLOG_API_KEY = $BACKLOG_API_KEY"
+
+CURL_OPTIONS="-s"
+
+function get_space() {
+  local path="/api/v2/space"
+  curl $CURL_OPTIONS $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
+}
+
+function get_space_licence() {
+  local path="/api/v2/space/licence"
+  curl $CURL_OPTIONS $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
+}
+
+function get_space_diskusage() {
+  local path="/api/v2/space/diskUsage"
+  curl $CURL_OPTIONS $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
+}
+
