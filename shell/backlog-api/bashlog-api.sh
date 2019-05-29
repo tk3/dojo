819e122ce355d930adff82d9a6867747f0118223
qtakamitsu <qtakamitsu@gmail.com>
2019-05-29 23:26:48 +0900

    update backlog-api.sh

--- shell/backlog-api/bashlog-api.sh
+++ shell/backlog-api/bashlog-api.sh
@@ -31,3 +31,14 @@ function delete_issues() {
   curl $CURL_OPTIONS --request DELETE $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
 }
 
+function count_issues() {
+  local path="/api/v2/issues/count"
+  curl $CURL_OPTIONS $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
+}
+
+function get_issue_comments() {
+  local issueIdOrKey="$1"
+  local path="/api/v2/issues/$issueIdOrKey/comments"
+  curl $CURL_OPTIONS $BACKLOG_URL$path?apiKey=$BACKLOG_API_KEY
+}
+
