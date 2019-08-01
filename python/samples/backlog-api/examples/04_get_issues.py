02b0813f6863488de5abd63983ae28f70711bff5
qtakamitsu <qtakamitsu@gmail.com>
2019-08-01 21:42:11 +0900

    Add examples

---
+++ python/samples/backlog-api/examples/04_get_issues.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
+backlog_url = os.getenv('BACKLOG_URL')
+api_key = os.getenv('BACKLOG_API_KEY')
+
+r = requests.get(
+        backlog_url + '/api/v2/issues',
+        params={
+            'apiKey': api_key
+            })
+
+print(json.dumps(r.json(), indent=4))
+
