e1b73179c0d3b907f86baa926f0b6eb7f937c0a7
tk3 <tk3inc@gmail.com>
2020-09-26 11:11:31 +0000

    update

--- cc/odbc/list_data_source/sample.c
+++ cc/odbc/list_data_source/sample.c
@@ -7,8 +7,8 @@ int main() {
   SQLHENV henv;
   SQLRETURN r;
 
-  SQLCHAR dsn_name[256];
-  SQLSMALLINT dsn_name_len;
+  SQLCHAR ds_name[256];
+  SQLSMALLINT ds_name_len;
   SQLCHAR driver_desc[256];
   SQLSMALLINT driver_desc_len;
   SQLUSMALLINT direction;
@@ -18,18 +18,17 @@ int main() {
   r = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (void *)SQL_OV_ODBC3, 0);
 
   direction = SQL_FETCH_FIRST;
-  r = SQLDataSources(henv, direction, dsn_name, sizeof(dsn_name), &dsn_name_len,
+  r = SQLDataSources(henv, direction, ds_name, sizeof(ds_name), &ds_name_len,
                      driver_desc, sizeof(driver_desc), &driver_desc_len);
   while (SQL_SUCCEEDED(r)) {
     printf("Data Source\n");
-    printf("  name: %.*s\n", dsn_name_len, dsn_name);
+    printf("  name: %.*s\n", ds_name_len, ds_name);
     printf("  description: %.*s\n", driver_desc_len, driver_desc);
     printf("\n");
 
     direction = SQL_FETCH_NEXT;
-    r = SQLDataSources(henv, direction, dsn_name, sizeof(dsn_name),
-                       &dsn_name_len, driver_desc, sizeof(driver_desc),
-                       &driver_desc_len);
+    r = SQLDataSources(henv, direction, ds_name, sizeof(ds_name), &ds_name_len,
+                       driver_desc, sizeof(driver_desc), &driver_desc_len);
   }
 
   if (henv != SQL_NULL_HENV) {
