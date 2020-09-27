605fb64b9ce0e8bb4806749a3fba6e23b689f071
tk3 <tk3inc@gmail.com>
2020-09-27 04:04:16 +0000

    update

--- cc/odbc/select/sample.c
+++ cc/odbc/select/sample.c
@@ -18,16 +18,19 @@ typedef struct _COLUMN_BINDING {
 	struct _COLUMN_BINDING *next;
 } COLUMN_BINDING;
 
+int print_message(SQLHANDLE *handle, SQLSMALLINT type, SQLRETURN ret);
 int column_bind(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING **binding);
 int column_binding_print(COLUMN_BINDING *binding);
+int execute_fetch(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING *binding);
 
-int main() {
+int main()
+{
 	SQLRETURN ret;
 	SQLHENV env;
 	SQLHDBC dbc;
 	SQLHSTMT stmt;
 	const char *conn_str = "DSN=mysqlitedb";
-	const char *sql_str = "select name, page as p from books;";
+	const char *sql_str = "select name as n, page from books;";
 	SQLSMALLINT num_results_col;
 	COLUMN_BINDING *binding;
 
@@ -52,6 +55,7 @@ int main() {
 
 		column_bind(stmt, num_results_col, &binding);
 		column_binding_print(binding);
+		execute_fetch(stmt, num_results_col, binding);
 	}
 	SQLFreeHandle(SQL_HANDLE_STMT, stmt);
 
@@ -66,6 +70,7 @@ int main() {
 int column_bind(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING **binding)
 {
 	int i;
+	SQLRETURN ret;
 	COLUMN_BINDING *current_binding;
 	COLUMN_BINDING *prev_binding = NULL;
 	SQLLEN length_or_indicator;
@@ -101,14 +106,47 @@ int column_bind(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING **bi
 
 		current_binding->value = (char *)malloc(current_binding->value_size + 1);
 
-		SQLBindCol(stmt,
+		if (current_binding->value_type == SQL_INTEGER ) {
+			current_binding->value_type = SQL_C_LONG;
+		} else if (current_binding->value_type == SQL_VARCHAR ) {
+			current_binding->value_type = SQL_C_CHAR;
+        }
+
+		ret = SQLBindCol(stmt,
 				i,
 				current_binding->value_type,
 				current_binding->value,
 				current_binding->value_size,
 				&length_or_indicator);
+		printf("SQLBindCol()\n");
+		print_message(stmt, SQL_HANDLE_STMT, ret);
 
 		prev_binding = current_binding;
+	}
+
+	return 0;
+}
+
+int print_message(SQLHANDLE *handle, SQLSMALLINT type, SQLRETURN ret)
+{
+	int i;
+	char state[SQL_SQLSTATE_SIZE + 1];
+	char message[255 + 1];
+	SQLINTEGER native_error;
+
+	if (ret == SQL_SUCCESS) {
+		return 0;
+	}
+
+	while (SQLGetDiagRec(type,
+				handle,
+				++i,
+				(SQLCHAR *)state,
+				&native_error,
+				(SQLCHAR *)message,
+				sizeof(message) - 1,
+				NULL) == SQL_SUCCESS) {
+		printf("[%5.5s] %s (%d)\n", state, message, native_error);
 	}
 
 	return 0;
@@ -117,6 +155,7 @@ int column_bind(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING **bi
 int column_binding_print(COLUMN_BINDING *binding)
 {
 	COLUMN_BINDING *current_binding;
+
 	for (current_binding = binding; current_binding; current_binding = current_binding->next) {
 		printf("name: %s\n", current_binding->name);
 		printf("data type: %d\n", current_binding->value_type);
@@ -124,6 +163,33 @@ int column_binding_print(COLUMN_BINDING *binding)
 		printf("decimal digits: %d\n", current_binding->decimal_digits);
 		printf("nullable: %d\n", current_binding->nullable);
 		puts("");
+	}
+
+	return 0;
+}
+
+int execute_fetch(SQLHSTMT *stmt, SQLSMALLINT num_results_col, COLUMN_BINDING *binding)
+{
+	int i;
+	SQLRETURN ret;
+	COLUMN_BINDING *current_binding;
+
+	for (i = 0; ; i++) {
+		ret = SQLFetch(stmt);
+		if (ret == SQL_NO_DATA) {
+			break;
+		}
+
+		for (current_binding = binding; current_binding; current_binding = current_binding->next) {
+			printf("name: %s\n", current_binding->name);
+
+			switch (current_binding->value_type) {
+			case SQL_C_LONG:
+				printf("value: %d\n", (int)(current_binding->value));
+			case SQL_C_CHAR:
+				printf("value: %s\n", current_binding->value);
+			}
+		}
 	}
 
 	return 0;
