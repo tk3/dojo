01e58adc9ee8e9392508e1f0f91fec1b24147637
tk3 <169007+tk3@users.noreply.github.com>
2026-04-28 21:41:17 +0900

    add BubbleSort project

---
+++ fsharp/BubbleSort/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let rnd = Random()
+    let randomArray = [| for i in 1 .. 10 -> rnd.Next(1, 101) |]
+
+    for i = 0 to randomArray.Length - 2 do
+        for j = 0 to randomArray.Length - 2 - i do
+            if randomArray[j] > randomArray[j + 1] then
+                let tmp = randomArray[j]
+                randomArray[j] <- randomArray[j + 1]
+                randomArray[j + 1] <- tmp
+
+    printfn "%A" randomArray
+    0
+
