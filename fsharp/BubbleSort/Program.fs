1ad584db8d7bb11b07b63a80bc89b46c111cc38e
tk3 <169007+tk3@users.noreply.github.com>
2026-05-24 17:21:20 +0900

    update

--- fsharp/BubbleSort/Program.fs
+++ fsharp/BubbleSort/Program.fs
@@ -1,7 +1,6 @@
 ﻿open System
 
-[<EntryPoint>]
-let main _ =
+let bubbleSort1 =
     let rnd = Random()
     let randomArray = [| for _ in 1..10 -> rnd.Next(1, 101) |]
 
@@ -11,6 +10,38 @@ let main _ =
                 let tmp = randomArray[j]
                 randomArray[j] <- randomArray[j + 1]
                 randomArray[j + 1] <- tmp
+    printfn "BubbleSort1 result: %A" randomArray
 
-    printfn "%A" randomArray
+let rec bubbleSort2 (list: 'T list) =
+    let rec bubblePass lst =
+        match lst with
+        | [] -> [], false
+        | [x] -> [x], false
+        | x :: y :: ys ->
+            if x > y then
+                let rest, _ = bubblePass (x ::ys)
+                y :: rest, true
+            else
+                let rest, swapped = bubblePass (y:: ys)
+                x :: rest, swapped
+
+    let sortedList, swapped = bubblePass list
+    if swapped then
+        bubbleSort2 sortedList
+    else
+        sortedList
+
+
+[<EntryPoint>]
+let main _ =
+
+    bubbleSort1
+
+    let rnd = Random()
+    let randomList = [ for _ in 1..10 -> rnd.Next(1, 101) ]
+
+    let sortedList = bubbleSort2 randomList
+    printfn "bubbleSort2       : %A" randomList
+    printfn "bubbleSort2 result: %A" sortedList
+
     0
